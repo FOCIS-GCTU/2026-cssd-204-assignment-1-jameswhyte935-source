73faf2415bf22ba53d56b405c4491ef82831ed3d
jameswhyte935-source <jameswhyte935@gmail.com>
2026-05-31 03:21:48 -0900

    Implement initials printing in main function
    Added a main function to print initials with formatting.

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: JWG.py
+
+#
+# Name: James Whyte Gyebi
+# STUDENT ID:2425401109
+# Email: 2425401109@live.gctu.edu.gh
+# Grader: Augutus Buckman
+#
+# On my honor, James Whyte Gyebi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
 
+    print()
+    print("...JWG")
+    print()
+    print("...JJJJJJJJJJJJ........WW........WW........GGGGGGGGGGGG....")
+    print("...JJJJJJJJJJJJ........WW........WW........GGGGGGGGGGGG....")
+    print("........JJJ............WW........WW........GGGG............")
+    print("........JJJ............WW........WW........GGGG............")
+    print("........JJJ............WW...WW...WW........GGGG..GGGGGG....")
+    print("........JJJ............WW...WW...WW........GGGG..GGGGGG....")
+    print("........JJJ............WW..WWWW..WW........GGGG....GGGG....")
+    print("...JJ...JJJ............WW..WWWW..WW........GGGG....GGGG....")
+    print("...JJ...JJJ.......**...WWWW....WWWW...**...GGGGGGGGGGGG..**")
+    print("...JJJJJJJJ.......**...WWWW....WWWW...**...GGGGGGGGGGGG..**")
 
+    print()
 
 main()
